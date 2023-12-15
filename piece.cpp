7b67c341ce55e1028252f91d528c2c80c35d836f
Beaurel Fohom <beaurelfohom79@gmail.com>
2023-12-14 23:30:27 -0500

    ajout type de piece et certaines méthodes

--- piece.cpp
+++ piece.cpp
@@ -1,50 +1,70 @@
 #include "piece.h"
 #include <string>
 
-Piece::Piece(int x, int y, Couleur couleur) : position(x, y), couleur(couleur) {};
+Piece::Piece(int x, int y, Couleur couleur) : position(x, y), ancienne_position(x, y), couleur(couleur) {}
 
 Position Piece::get_position() const{
     return position;
 }
 
-bool Piece::position_valide(const Position& position, array<array<Piece*, 8>, 8> pieces) const{ //utiliser par positions_possibles
-    return not ((pieces[position.getY()][position.getX()] != nullptr) and
+Position Piece::get_ancienne_position() const
+{
+    return ancienne_position;
+}
+
+bool Piece::position_valide(const Position& position, const array<array<Piece*, 8>, 8>& pieces) const{ //utiliser par positions_possibles
+    return !((pieces[position.getY()][position.getX()] != nullptr) &&
             (pieces[position.getY()][position.getX()]->get_couleur() == this->get_couleur()));
 }
 
 // verifier si le deplacement est valide
-bool Piece::deplacement_valide(int x, int y, array<array<Piece*, 8>, 8> pieces) const{
+bool Piece::deplacement_valide(int x, int y, const array<array<Piece*, 8>, 8>& pieces) const
+{
     // verifie si le couple (x,y) est une position possible lors du prochain deplacement
     for (auto const& pos : positions_possibles(pieces))
     {
-        if((x == pos.getX()) and (y == pos.getY())){
+        if(pos.egale(x, y)){
             return true;
         }
     }
     return false;
-};
+}
 
-bool Piece::manger(const Position& position,  array<array<Piece*, 8>, 8> pieces) const{
-    //Utiliser dans game.cpp pour marquer les pieces pouvant etre manger par une couleur rouge
-    return ((pieces[position.getY()][position.getX()] != nullptr) and
-            (pieces[position.getY()][position.getX()]->get_couleur() != this->get_couleur()) and
+bool Piece::peut_manger(const Position& position,  const array<array<Piece*, 8>, 8>& pieces) const
+{
+    return ((pieces[position.getY()][position.getX()] != nullptr) &&
+            (pieces[position.getY()][position.getX()]->get_couleur() != this->get_couleur()) &&
             (position.egale(pieces[position.getY()][position.getX()]->get_position())));
 }
 
-Couleur Piece::get_couleur() const {
+Couleur Piece::get_couleur() const
+{
     return couleur;
-};
+}
 
-string Piece::get_image() const {
+string Piece::get_image() const
+{
     return image;
 }
 
-// deplacer piece (utilise deplacement_valide)
-void Piece::se_deplacer(int x, int y, array<array<Piece*, 8>, 8> pieces)
+Type Piece::get_type() const
 {
-    if (deplacement_valide(x, y, pieces)){
+    return type;
+}
+
+int Piece::get_y_init() const
+{
+    return 0;
+}
+
+// deplacer piece (utilise deplacement_valide)
+void Piece::se_deplacer(int x, int y, const array<array<Piece*, 8>, 8>& pieces)
+{
+    if (deplacement_valide(x, y, pieces))
+    {
+        ancienne_position = position;
         position.modifier_position(x, y);
     }
-};
+}
 
-Piece::~Piece(){};
+Piece::~Piece(){}
--- piece.h
+++ piece.h
@@ -1,10 +1,10 @@
+#ifndef _PIECE_
+#define _PIECE_
 #include <vector>
 #include "position.h"
 #include "setting.h"
 #include <array>
 using namespace std;
-#ifndef _PIECE_
-#define _PIECE_
 class Piece
 {
 public:
@@ -12,27 +12,36 @@ public:
     Piece(int x, int y, Couleur couleur);
     virtual ~Piece();
 
-    void se_deplacer(int x, int y, array<array<Piece*, 8>, 8> pieces); // deplacer piece (utilise deplacement_valide) et sa nouvelle position devient x, y
+    void se_deplacer(int x, int y, const array<array<Piece*, 8>, 8>& pieces); // deplacer piece (utilise deplacement_valide) et sa nouvelle position devient x, y
 
-    virtual vector<Position> positions_possibles(array<array<Piece*, 8>, 8> pieces) const = 0; // differentes positions que la piece peut occuper si elle se deplace
+    virtual vector<Position> positions_possibles(const array<array<Piece*, 8>, 8>& pieces) const = 0; // differentes positions que la piece peut occuper si elle se deplace
 
-    virtual bool position_valide(const Position& position, array<array<Piece*, 8>, 8> pieces) const; // verifie si la position ne correspond pas a la position
+    virtual bool position_valide(const Position& position, const array<array<Piece*, 8>, 8>& pieces) const; // verifie si la position ne correspond pas a la position
                                                                                              // d'un autre pion du meme joueur
+    virtual int get_y_init() const;
 
-    bool deplacement_valide(int x, int y, array<array<Piece*, 8>, 8> pieces) const; // verifier si la position x, y est valide (figure parmi les positions_possibles)
+    bool deplacement_valide(int x, int y, const array<array<Piece*, 8>, 8>& pieces) const; // verifier si la position x, y est valide (figure parmi les positions_possibles)
 
-    bool manger(const Position& position,  array<array<Piece*, 8>, 8> pieces) const; // renvoie vrai si on peut manger une piece a cette position
+    //bool deplacement_valide_avec_echec(int x, int y, array<array<Piece*, 8>, 8> pieces) const;
+
+    bool peut_manger(const Position& position,  const array<array<Piece*, 8>, 8>& pieces) const; // renvoie vrai si on mange une piece a cette position
 
     Position get_position() const; //retourne la position de la piece
+
+    Position get_ancienne_position() const;
 
     Couleur get_couleur() const;
 
     string get_image() const;
 
+    Type get_type() const;
+
 protected:
     Position position;
+    Position ancienne_position;
     Couleur couleur;
     string image;
+    Type type;
 };
 
 #endif
